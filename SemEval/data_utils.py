f0b5de38babf209820886f19723e7d0e16e8177c
StarWang <280220970@qq.com>
2018-03-24 12:32:51 -0400

    add padding tool for single sequence in a batch

--- SemEval/data_utils.py
+++ SemEval/data_utils.py
@@ -2,26 +2,33 @@ import torch
 import random
 import numpy as np
 import json
+import torch
+from torch.autograd import Variable
+from torch import LongTensor, FloatTensor, ByteTensor
+from unicodedata import normalize
 from collections import defaultdict
 
 class Sample():
+    # property: id, d_words, q_words, c_words, label, d_q_relation, d_c_relation, d_pos, q_pos, d_ner, features
     def __init__(self, info, word_dict, pos_dict, ne_dict, relation_dict):
         # concatenation of dataset id (trial/train/dev/test), document id,
         # question id and choice id
         self.id = info['id']
 
         # text in the document, question and choice
-        self.doc = [word_dict[w] for w in info['d_words']]
-        self.question = [word_dict[w] for w in info['q_words']]
-        self.choice = [word_dict[w] for w in info['c_words']]
+        # normalize: characters are decomposed by canonical equivalence,
+        # and multiple combining characters are arranged in a specific order
+        self.d_words = [word_dict[normalize('NFD', w)] for w in info['d_words']]
+        self.q_words = [word_dict[normalize('NFD', w)] for w in info['q_words']]
+        self.c_words = [word_dict[normalize('NFD', w)] for w in info['c_words']]
 
         # label, for test set, the label is always -1
         self.label = info['label']
 
         ## features
         # for each word in the document, whether it appears in the question/choice
-        self.in_q = info['in_q']
-        self.in_c = info['in_c']
+        in_q = info['in_q']
+        in_c = info['in_c']
 
         # named entity for each document word
         self.d_ner = [ne_dict[w] for w in info['d_ner']]
@@ -35,16 +42,32 @@ class Sample():
         self.q_pos = [pos_dict[w] for w in info['q_pos']]
 
         # term frequency
-        self.tf = info['tf']
+        tf = info['tf']
 
         # lemma features (not mentioned in paper, no idea how it's crafted)
-        self.lemma_in_q = info['lemma_in_q']
-        self.lemma_in_c = info['lemma_in_c']
+        # guess is the lemmatized version of in_q/in_c
+        lemma_in_q = info['lemma_in_q']
+        lemma_in_c = info['lemma_in_c']
 
-def build_dict():
+        # stack features
+        self.features = [
+                 in_q, in_c,
+                 lemma_in_q, lemma_in_c,
+                 tf
+                ]
+
+
+def build_dict(type):
     dct = defaultdict(lambda :len(dct))
+    # NULL: used for padding
     dct['<NULL>'] = 0
+    # UNK: unknown word
     dct['<UNK>'] = 1
+    file_path = {'word':'./data/vocab', 'pos':'./data/pos_vocab', 'ne': './data/ner_vocab', 'relation':'./data/rel_vocab'}
+    with open(file_path[type], 'r', encoding='utf-8') as f:
+        for line in f:
+            word = line.strip('\n')
+            dct[normalize('NFD', word)]
     return dct
 
 def set_seed(seed):
@@ -52,10 +75,63 @@ def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
 
-def load_data(path):
+def load_data(path, word_dict, pos_dict, ne_dict, relation_dict):
     with open(path, 'r', encoding='utf-8') as f:
-        return [Sample(json.loads(sample.strip('\n'))) for sample in f]
+        return [Sample(json.loads(sample.strip('\n')), word_dict, pos_dict, ne_dict, relation_dict) for sample in f]
 
+def get_batches(data, batch_size):
+    for i in range(len(data), batch_size):
+        batch = pad_batch(data[i:i + batch_size])
+        yield batch
+
+def pad_batch_by_sequence(batch_seq, dtype):
+    batch_size = len(batch_seq)
+    max_len = max([len(seq) for seq in batch_seq])
+
+    # for word sequence, need a mask to extract the original words to when performing attention
+    mask = np.ones((batch_size, max_len))
+    padded_batch = np.zeros((batch_size, max_len))
+    for i, seq in enumerate(batch_seq):
+        padded_batch[i, :len(seq)] = seq
+        mask_batch[i, :len(seq)] = 0
+    return Variable(dtype(padded_batch)), Variable(ByteTensor(mask))
+
+def pad_batch_by_sequence_list(batch_seq, dtypes):
+    f
+
+def pad_batch(batch_data):
+    # sample property: id, d_words, q_words, c_words, label, d_q_relation, d_c_relation, d_pos, q_pos, d_ner, features
+    # data to pad: word, pos, ne, relation, features
+    q_words, q_mask = pad_batch_by_sequence([s.q_words for s in batch_data], LongTensor)
+    d_words, d_mask = pad_batch_by_sequence([s.d_words for s in batch_data], LongTensor)
+    c_words, c_mask = pad_batch_by_sequence([s.c_words for s in batch_data], LongTensor)
+
+    d_q_relation, _ = pad_batch_by_sequence([s.d_q_relation for s in batch_data], LongTensor)
+    d_c_relation, _ = pad_batch_by_sequence([s.d_c_relation for s in batch_data], LongTensor)
+
+    q_pos, _ = pad_batch_by_sequence([s.q_pos for s in batch_data], LongTensor)
+    d_pos, _ = pad_batch_by_sequence([s.d_pos for s in batch_data], LongTensor)
+
+    d_ner, _ = pad_batch_by_sequence([s.d_ner for s in batch_data], LongTensor)
+
+    features_types = [ByteTensor]*4 + [FloatTensor]
+    features , _ = pad_batch_by_sequence_list([s.features for s in batch_data], features_types)
+
+    label = pad_batch_by_sequence([s.label for s in batch_data], FloatTensor)
+
+    return {
+            'q_words':q_words,
+            'q_mask':q_mask,
+            'd_words':d_words,
+            'd_mask':d_mask,
+            'd_q_relation':d_q_relation,
+            'd_c_relation':d_c_relation,
+            'q_pos':q_pos,
+            'd_pos':d_pos,
+            'd_ner':d_ner,
+            'featuers':features,
+            'label':label
+            }
 
 '''
 def filter_embedding(embedding_path, word_dict, save_path):
--- SemEval/main.py
+++ SemEval/main.py
@@ -2,7 +2,7 @@ import yaml
 import torch
 import random
 import numpy as np
-from data_utils import set_seed, load_data
+from data_utils import set_seed, load_data, build_dict
 
 
 if __name__ == '__main__':
@@ -11,26 +11,33 @@ if __name__ == '__main__':
 
     set_seed(config['seed'])
 
+    # get word2ind dictionary, order: word, pos, ne, relation
+    w2i_lst = []
+    for t in ['word', 'pos', 'ne', 'relation']:
+        w2i_lst.append(build_dict(t))
+
     # get word2ind/ind2word or feature2ind/ind2feature dictionary
 
     # load train data
-    train_data = load_data('./data/train-data-processed.json')
+    train_data = load_data('./data/train-data-processed.json', *w2i_lst)
 
     # load trial data
-    trial_data = load_data('./data/trial-data-processed.json')
+    trial_data = load_data('./data/trial-data-processed.json', *w2i_lst)
 
     # concatenate train data and trial data
     train_data += trail_data
 
     # load dev data
-    dev_data = load_data('./data/dev-data-processed.json')
+    dev_data = load_data('./data/dev-data-processed.json', *w2i_lst)
 
     # load test data
-    test_data = load_data('./data/test-data-processed.json')
+    test_data = load_data('./data/test-data-processed.json', *w2i_lst)
 
     for epoch in range(config['epoch']):
         train_data = random.shuffle(train_data)
         # get data in batch
+        for batch_data in get_batches(train_data):
+            f
 
 
         # train_model
