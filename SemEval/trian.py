2b435abba6731d1469488b9cf6af2faebc79c6cc
StarWang <280220970@qq.com>
2018-03-25 07:54:08 -0400

    triAn -> triAN

--- SemEval/trian.py
+++ SemEval/trian.py
@@ -5,16 +5,16 @@ import torch.nn.functional as F
 from layers import StackedBiLSTM, SequenceAttentionMM, SequenceAttentionMV, SelfAttention, AllEmbedding
 from utils import vocab, pos_vocab, ner_vocab, rel_vocab
 
-class TriAn(nn.module):
+class TriAN(nn.module):
     def __init__(self, args):
-        super(TriAn, self).__init__()
+        super(TriAN, self).__init__()
         self.args = args
         # 0. Embedding layer
         self.embeddings = AllEmbedding(len(vocab), len(pos_vocab), len(ner_vocab), len(rel_vocab), args.embedding_dim, args.pos_emb_dim, args.ner_emb_dim, args.rel_emb_dim, args.emb_dropout)
         self.p_q_embedder = self.SequenceAttentionMM(self.args.embedding_dim, self.args.embedding_dim)
         self.c_q_embedder = self.SequenceAttentionMM(self.args.embedding_dim, self.args.embedding_dim)
         self.c_p_embedder = self.SequenceAttentionMM(self.args.embedding_dim, self.args.embedding_dim)
-        
+
         """
         passage = [p_embed, p_q_embed, p_pos_embed, p_ner_embed, p_q_rel_embed, p_c_rel_embed, f_tensor]
         question = [q_embed, q_pos_embed]
@@ -46,19 +46,19 @@ class TriAn(nn.module):
         p_q_emb = self.p_q_embedder(p_emb, q_emb, q_mask)
         c_q_emb = self.c_q_embedder(c_emb, q_emb, q_mask)
         c_p_emb = self.c_p_embedder(c_emb, p_emb, c_mask)
-        
+
         p_rnn_in = torch.cat([p_emb, p_q_emb, p_pos_emb, p_ner_emb, f_tensor], dim = 2)
         q_rnn_in = torch.cat([q_emb, q_pos_emb], dim = 2)
         c_rnn_in = torch.cat([c_emb, c_q_emb, c_p_emb], dim = 2)
-        
+
         p_rnn_out = self.p_rnn(p_rnn_in, p_mask)
         q_rnn_out = self.q_rnn(q_rnn_in, q_mask)
         c_rnn_out = self.c_rnn(c_rnn_in, c_mask)
-        
+
         q_attn_out = self.q_qAttn(q_rnn_out)
         p_attn_out = self.p_qAttn(p_rnn_out, q_attn_out)
         c_attn_out = self.c_cAttn(c_rnn_out)
-        
+
         preactivation = (self.p_c_interact(p_attn_out) + self.q_c_interact(q_attn_out))*c_attn_out
         preactivation = torch.sum(preactivation, dim = -1)
         return F.sigmoid(preactivation)
