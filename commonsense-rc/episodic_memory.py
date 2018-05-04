3aa49b0b2568f17cfcb5f33000caa2fcdbb2daba
StarWang <280220970@qq.com>
2018-05-04 19:57:12 -0400

    add grid search and output

---
+++ commonsense-rc/episodic_memory.py
@@ -0,0 +1,218 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+
+def position_encoding(embedded_sentence):
+    '''
+    embedded_sentence.size() -> (#batch, #sentence, #token, #embedding)
+    l.size() -> (#sentence, #embedding)
+    output.size() -> (#batch, #sentence, #embedding)
+    '''
+    _, _, slen, elen = embedded_sentence.size()
+
+    l = [[(1 - s/(slen-1)) - (e/(elen-1)) * (1 - 2*s/(slen-1)) for e in range(elen)] for s in range(slen)]
+    l = torch.FloatTensor(l)
+    l = l.unsqueeze(0) # for #batch
+    l = l.unsqueeze(1) # for #sen
+    l = l.expand_as(embedded_sentence)
+    weighted = embedded_sentence * Variable(l.cuda())
+    return torch.sum(weighted, dim=2).squeeze(2) # sum with tokens
+
+class AttentionGRUCell(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(AttentionGRUCell, self).__init__()
+        self.hidden_size = hidden_size
+        self.Wr = nn.Linear(input_size, hidden_size)
+        init.xavier_normal(self.Wr.state_dict()['weight'])
+        self.Ur = nn.Linear(hidden_size, hidden_size)
+        init.xavier_normal(self.Ur.state_dict()['weight'])
+        self.W = nn.Linear(input_size, hidden_size)
+        init.xavier_normal(self.W.state_dict()['weight'])
+        self.U = nn.Linear(hidden_size, hidden_size)
+        init.xavier_normal(self.U.state_dict()['weight'])
+
+    def forward(self, fact, C, g):
+        '''
+        fact.size() -> (#batch, #hidden = #embedding)
+        c.size() -> (#hidden, ) -> (#batch, #hidden = #embedding)
+        r.size() -> (#batch, #hidden = #embedding)
+        h_tilda.size() -> (#batch, #hidden = #embedding)
+        g.size() -> (#batch, )
+        '''
+
+        r = F.sigmoid(self.Wr(fact) + self.Ur(C))
+        h_tilda = F.tanh(self.W(fact) + r * self.U(C))
+        g = g.unsqueeze(1).expand_as(h_tilda)
+        h = g * h_tilda + (1 - g) * C
+        return h
+
+class AttentionGRU(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(AttentionGRU, self).__init__()
+        self.hidden_size = hidden_size
+        self.AGRUCell = AttentionGRUCell(input_size, hidden_size)
+
+    def forward(self, facts, G):
+        '''
+        facts.size() -> (#batch, #sentence, #hidden = #embedding)
+        fact.size() -> (#batch, #hidden = #embedding)
+        G.size() -> (#batch, #sentence)
+        g.size() -> (#batch, )
+        C.size() -> (#batch, #hidden)
+        '''
+        batch_num, sen_num, embedding_size = facts.size()
+        C = Variable(torch.zeros(self.hidden_size)).cuda()
+        for sid in range(sen_num):
+            fact = facts[:, sid, :]
+            g = G[:, sid]
+            if sid == 0:
+                C = C.unsqueeze(0).expand_as(fact)
+            C = self.AGRUCell(fact, C, g)
+        return C
+
+class EpisodicMemory(nn.Module):
+    def __init__(self, hidden_size):
+        super(EpisodicMemory, self).__init__()
+        self.AGRU = AttentionGRU(hidden_size, hidden_size)
+        self.z1 = nn.Linear(4 * hidden_size, hidden_size)
+        self.z2 = nn.Linear(hidden_size, 1)
+        self.next_mem = nn.Linear(3 * hidden_size, hidden_size)
+        init.xavier_normal(self.z1.state_dict()['weight'])
+        init.xavier_normal(self.z2.state_dict()['weight'])
+        init.xavier_normal(self.next_mem.state_dict()['weight'])
+
+    def make_interaction(self, facts, questions, prevM):
+        '''
+        facts.size() -> (#batch, #sentence, #hidden = #embedding)
+        questions.size() -> (#batch, 1, #hidden)
+        prevM.size() -> (#batch, #sentence = 1, #hidden = #embedding)
+        z.size() -> (#batch, #sentence, 4 x #embedding)
+        G.size() -> (#batch, #sentence)
+        '''
+        batch_num, sen_num, embedding_size = facts.size()
+        questions = questions.expand_as(facts)
+        prevM = prevM.expand_as(facts)
+
+        z = torch.cat([
+            facts * questions,
+            facts * prevM,
+            torch.abs(facts - questions),
+            torch.abs(facts - prevM)
+        ], dim=2)
+
+        z = z.view(-1, 4 * embedding_size)
+
+        G = F.tanh(self.z1(z))
+        G = self.z2(G)
+        G = G.view(batch_num, -1)
+        G = F.softmax(G, dim=1)
+
+        return G
+
+    def forward(self, facts, questions, prevM):
+        '''
+        facts.size() -> (#batch, #sentence, #hidden = #embedding)
+        questions.size() -> (#batch, #sentence = 1, #hidden)
+        prevM.size() -> (#batch, #sentence = 1, #hidden = #embedding)
+        G.size() -> (#batch, #sentence)
+        C.size() -> (#batch, #hidden)
+        concat.size() -> (#batch, 3 x #embedding)
+        '''
+        G = self.make_interaction(facts, questions, prevM)
+        C = self.AGRU(facts, G)
+        concat = torch.cat([prevM.squeeze(1), C, questions.squeeze(1)], dim=1)
+        next_mem = F.relu(self.next_mem(concat))
+        next_mem = next_mem.unsqueeze(1)
+        return next_mem
+
+
+class QuestionModule(nn.Module):
+    def __init__(self, hidden_size):
+        super(QuestionModule, self).__init__()
+        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
+
+    def forward(self, questions, word_embedding):
+        '''
+        questions.size() -> (#batch, #token)
+        word_embedding() -> (#batch, #token, #embedding)
+        gru() -> (1, #batch, #hidden)
+        '''
+        questions = word_embedding(questions)
+        _, questions = self.gru(questions)
+        questions = questions.transpose(0, 1)
+        return questions
+
+class InputModule(nn.Module):
+    def __init__(self, hidden_size):
+        super(InputModule, self).__init__()
+        self.hidden_size = hidden_size
+        self.gru = nn.GRU(300, hidden_size, bidirectional=True, batch_first=True)
+        for name, param in self.gru.state_dict().items():
+            if 'weight' in name: init.xavier_normal(param)
+        self.dropout = nn.Dropout(0.3)
+
+    def forward(self, contexts, word_embedding):
+        '''
+        contexts.size() -> (#batch, #sentence, #token)
+        word_embedding() -> (#batch, #sentence x #token, #embedding)
+        position_encoding() -> (#batch, #sentence, #embedding)
+        facts.size() -> (#batch, #sentence, #hidden = #embedding)
+        '''
+        batch_num, sen_num, token_num = contexts.size()
+
+        contexts = contexts.view(batch_num, -1)
+        contexts = word_embedding(contexts)
+
+        contexts = contexts.view(batch_num, sen_num, token_num, -1)
+        contexts = position_encoding(contexts)
+        contexts = self.dropout(contexts)
+
+        h0 = Variable(torch.zeros(2, batch_num, self.hidden_size).cuda())
+        facts, hdn = self.gru(contexts, h0)
+        facts = facts[:, :, :self.hidden_size] + facts[:, :, self.hidden_size:]
+        return facts
+
+class DMNPlus(nn.Module):
+    def __init__(self, hidden_size, num_hop=1):
+        super(DMNPlus, self).__init__()
+        self.num_hop = num_hop
+
+        self.input_module = InputModule(hidden_size)
+        self.question_module = QuestionModule(hidden_size)
+        self.memory = EpisodicMemory(hidden_size)
+
+    def forward(self, contexts, questions, word_embedding):
+        '''
+        contexts.size() -> (#batch, #sentence, #token) -> (#batch, #sentence, #hidden = #embedding)
+        questions.size() -> (#batch, #token) -> (#batch, 1, #hidden)
+        '''
+        facts = self.input_module(contexts, word_embedding)
+        questions = self.question_module(questions, word_embedding)
+        M = questions
+        for hop in range(self.num_hop):
+            M = self.memory(facts, questions, M)
+        return M.squeeze()
+
+class DMNPlus2(nn.Module):
+    def __init__(self, hidden_size, num_hop=1):
+        super(DMNPlus2, self).__init__()
+        self.num_hop = num_hop
+
+        self.input_module = InputModule(hidden_size)
+        self.memory = EpisodicMemory(hidden_size)
+
+    def forward(self, contexts, questions, word_embedding):
+        '''
+        contexts.size() -> (#batch, #sentence, #token) -> (#batch, #sentence, #hidden = #embedding)
+        questions.size() -> (#batch, #hidden) might need unsqueeze(1)
+        '''
+        facts = self.input_module(contexts, word_embedding)
+        questions = questions.unsqueeze(1)
+        M = Variable(torch.zeros(questions.size()).cuda())
+        for hop in range(self.num_hop):
+            M = self.memory(facts, questions, M)
+        return M.squeeze()
---
+++ commonsense-rc/main.py
@@ -0,0 +1,104 @@
+import os
+import time
+import torch
+import random
+import numpy as np
+
+from datetime import datetime
+
+import pandas as pd
+from utils import load_data, build_vocab, gen_submission, gen_final_submission
+from get_sequence import getTitle, readScriptKnowledge
+from config import args
+from model import Model
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+def main(output_prefix):
+    scriptKnowledge, _ = getTitle()
+    trainScriptKnowledge = readScriptKnowledge('data/train_script.txt', scriptKnowledge)
+    trialScriptKnowledge = readScriptKnowledge('data/trial_script.txt', scriptKnowledge)
+    devScriptKnowledge = readScriptKnowledge('data/dev_script.txt', scriptKnowledge)
+    testScriptKnowledge = readScriptKnowledge('data/test_script.txt', scriptKnowledge)
+
+    build_vocab()
+    train_data = load_data('./data/train-data-processed.json', trainScriptKnowledge, args.use_script)
+    train_data += load_data('./data/trial-data-processed.json', trialScriptKnowledge, args.use_script)
+    dev_data = load_data('./data/dev-data-processed.json', devScriptKnowledge, args.use_script)
+    if args.test_mode:
+        # use validation data as training data
+        train_data += dev_data
+        dev_data = []
+    model = Model(args)
+
+    best_dev_acc = 0.0
+    os.makedirs('./checkpoint', exist_ok=True)
+    checkpoint_path = './checkpoint/%d-%s.mdl' % (args.seed, datetime.now().isoformat())
+    print('Trained model will be saved to %s' % checkpoint_path)
+
+    for i in range(args.epoch):
+        print('Epoch %d...' % i)
+        if i == 0:
+            dev_acc = model.evaluate(dev_data)
+            print('Dev accuracy: %f' % dev_acc)
+        start_time = time.time()
+        np.random.shuffle(train_data)
+        cur_train_data = train_data
+
+        model.train(cur_train_data)
+        train_acc = model.evaluate(train_data[:2000], debug=False, eval_train=True)
+        print('Train accuracy: %f' % train_acc)
+        dev_acc = model.evaluate(dev_data, debug=True)
+        print('Dev accuracy: %f' % dev_acc)
+
+        if dev_acc > best_dev_acc:
+            best_dev_acc = dev_acc
+            #os.system('mv ./data/output.log ./data/best-dev.log')
+            model.save(checkpoint_path)
+        elif args.test_mode:
+            model.save(checkpoint_path)
+        print('Best dev accuracy: %f' % best_dev_acc)
+        print('Epoch %d use %d seconds.' % (i, time.time() - start_time))
+
+    dev_data = load_data('./data/test-data-processed.json', testScriptKnowledge, args.use_script)
+    model_path_list = [checkpoint_path]
+    os.system('rm ./out-*.txt')
+    for model_path in model_path_list:
+        print('Load model from %s...' % model_path)
+        args.pretrained = model_path
+        model = Model(args)
+
+        # evaluate on development dataset
+        dev_acc = model.evaluate(dev_data, output_prefix)
+        print('dev accuracy: %f' % dev_acc)
+
+        # generate submission zip file for Codalab
+        prediction = model.predict(dev_data)
+        gen_submission(dev_data, prediction)
+
+    gen_final_submission(dev_data, output_prefix)
+    print('Best dev accuracy: %f' % best_dev_acc)
+    return best_dev_acc
+
+if __name__ == '__main__':
+    stat = []
+    for seed in [1234, 123, 12, 1]:
+        for drop_out in [0.4, 0.3, 0.2, 0.1]:
+            for num_hop in [1, 2, 3]:
+                for use_script in [True, False]:
+                    set_seed(seed)
+                    args.seed = seed
+                    args.num_hop = num_hop
+                    args.dropout_rnn_output = drop_out
+                    args.dropout_emb = drop_out
+                    args.use_script = use_script
+                    args.pretrained = ''
+                    accuracy = main('seed_{}_drop_out_{}_num_hop_{}_use_script_{}'.format(seed, drop_out, num_hop, use_script))
+                    stat.append([seed, drop_out, accuracy])
+                    print (pd.DataFrame(stat, columns=['seed', 'drop_out', 'dev_acc']))
+
+    np.save('statistics', stat)
+
