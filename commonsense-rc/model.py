16c2fa52fd38ad35f0d2e05d9ff632b7faf73a4a
StarWang <280220970@qq.com>
2018-05-05 17:58:40 -0400

    debugging

--- commonsense-rc/model.py
+++ commonsense-rc/model.py
@@ -75,7 +75,6 @@ class Model:
                 pred_proba2, _ = self._get_bce_loss(batch_input[1])
                 y2 = self._zero_to_minus_one(batch_input[1][-1])
                 loss = torch.mean(-(y1*torch.log(pred_proba1 + 1e-10) + y2*torch.log(pred_proba2 + 1e-10)))
-                print (loss)
             else:
                 _, loss = self._get_bce_loss(batch_input)
             self.optimizer.zero_grad()
